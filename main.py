356aa9fedbb8bb4c42a0688f4597beaa835c953c
SebGeek <famille.auray@gmail.com>
2021-04-11 09:48:28 +0200

    Mise à jour main.ts, main.py

--- main.py
+++ main.py
@@ -5,20 +5,30 @@ from microbit import *
 # blue              : MOSI       : P15
 # green             : SCK        : P13
 # red               : VCC        : Not Connected
-
-basic.show_icon(IconNames.HEART)
+#
+# WS2801 needs 500 micro seconds between flushes
 
 pins.spi_frequency(1000000)
 pins.spi_format(8, 3)
 
 led_count = 96
+
 while True:
 
-    color = (50, 0, 50) # RGB
+    color = (5, 5, 5) # RGB
     for i in range(led_count):
         pins.spi_write(color[0])
         pins.spi_write(color[1])
         pins.spi_write(color[2])
-
-    # WS2801 needs 500 micro seconds between flushes
     control.wait_micros(500)
+    basic.clear_screen()
+    basic.pause(1000)
+
+    color = (8, 8, 8) # RGB
+    for i in range(led_count):
+        pins.spi_write(color[0])
+        pins.spi_write(color[1])
+        pins.spi_write(color[2])
+    control.wait_micros(500)
+    basic.show_icon(IconNames.HEART)
+    basic.pause(1000)
--- main.ts
+++ main.ts
@@ -1,21 +1,34 @@
 let color: number[];
+let i: number;
 //  WS2801 LED strip  : definition : microbit
 //  white             : GND        : GND
 //  blue              : MOSI       : P15
 //  green             : SCK        : P13
 //  red               : VCC        : Not Connected
-basic.showIcon(IconNames.Heart)
+// 
+//  WS2801 needs 500 micro seconds between flushes
 pins.spiFrequency(1000000)
 pins.spiFormat(8, 3)
 let led_count = 96
 while (true) {
-    color = [50, 0, 50]
+    color = [5, 5, 5]
     //  RGB
-    for (let i = 0; i < led_count; i++) {
+    for (i = 0; i < led_count; i++) {
         pins.spiWrite(color[0])
         pins.spiWrite(color[1])
         pins.spiWrite(color[2])
     }
-    //  WS2801 needs 500 micro seconds between flushes
     control.waitMicros(500)
+    basic.clearScreen()
+    basic.pause(1000)
+    color = [8, 8, 8]
+    //  RGB
+    for (i = 0; i < led_count; i++) {
+        pins.spiWrite(color[0])
+        pins.spiWrite(color[1])
+        pins.spiWrite(color[2])
+    }
+    control.waitMicros(500)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
 }
